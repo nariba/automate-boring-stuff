7f37d75bd18194f0eecbe7bf78a4cd836558ed5e
nariba <nariba@users.noreply.github.com>
2020-09-21 16:56:52 +0900

    add_to_inventory

--- inventory.py
+++ inventory.py
@@ -3,6 +3,8 @@
 stuff = \
     {'allow' : 12, 'gold coin' : 42, 'rope' : 1, 'torch' : 6, 'shuriken' : 1}
 
+dragon_loot = ['gold coin', 'shuriken', 'gold coin', 'gold coin', 'ruby']
+
 def display_inventory(stuff):
     count = 0
     for k, v in stuff.items():
@@ -10,5 +12,12 @@ def display_inventory(stuff):
         count += v
     print('The number of inventory : ' + str(count))
 
+def add_to_inventory(inventory, added_items):
+    for i in added_items:
+        inventory.setdefault(i, 0)
+        inventory[i] += 1
+
 if __name__ == "__main__":
+    display_inventory(stuff)
+    add_to_inventory(stuff, dragon_loot)
     display_inventory(stuff)
